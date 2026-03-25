fef41802f242ec12fe02b9d05153648e732f14da
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2026-03-25 10:45:36 -0400

    backup

---
+++ devenv.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.gdal
+  ];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
